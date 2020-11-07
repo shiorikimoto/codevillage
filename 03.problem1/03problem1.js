e8e39b57b21f9d6676dddb5d461eec9240881560
Shiori Kimoto <72813760+shiorikimoto@users.noreply.github.com>
2020-11-07 22:56:40 +0900

    Add files via upload

---
+++ 03.problem1/03problem1.js
@@ -0,0 +1,33 @@
+//10未満の自然数のうち, 3 もしくは 5 の倍数になっているものは 
+//3, 5, 6, 9 の4つがあり, これらの合計は 23 になる.
+//同じようにして, 1000 未満の 3 か 5 の倍数になっている数字の合計を求めよ.
+
+
+let sum = 0;
+for (let i = 3; i < 1000; i = i + 3) {
+  sum = sum + i;
+}
+
+let sum2 = 0;
+for (let i = 5; i < 1000; i = i + 5) {
+  sum2 = sum2 + i;
+}
+
+console.log(sum + sum2)
+
+
+
+
+
+//let sum = 0;
+//for (let i = 3; i < 10; i = i + 3) {
+//  sum = sum + i;
+//}
+
+//let sum2 = 0;
+//for (let i = 5; i < 10; i = i + 5) {
+//  sum2 = sum2 + i;
+//}
+
+//console.log(sum + sum2)
+
